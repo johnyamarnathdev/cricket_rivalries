2a23eb272095b2b43b0701470bf4d8ae317ae705
unknown <38614657+amardev19@users.noreply.github.com>
2019-02-24 17:56:27 -0500

    Initial angular and material setup

---
+++ ui/src/app/material.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+
+import {
+  MatButtonModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule 
+} from '@angular/material';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FlexLayoutModule 
+  ],
+  exports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FlexLayoutModule 
+  ]
+})
+export class MaterialModule {}
